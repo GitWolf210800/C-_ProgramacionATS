4e89cc0e84a75ca3b9de4eb11b89b33e3f127461
GitWolf210800 <ezad821q@hotmail.com>
2025-07-06 19:54:39 -0700

    primer programa en C++

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Primer programa en C++
+
+#include <iostream> //libreria para entrada y salida de datos
+
+using namespace std;
+
+int main() {
+
+    cout<<"Hola Mundo :D"<<endl;
+
+    return 0;
+}
