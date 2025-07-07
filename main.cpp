3cde0dff7213f973375375ed11e017cbebe66521
GitWolf210800 <ezad821q@hotmail.com>
2025-07-06 20:17:39 -0700

    segundo programa en C++ Tipos de datos básicos

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Primer programa en C++
+//Segundo programa en C++ tipos de datos básicos
 
 #include <iostream> //libreria para entrada y salida de datos
 
@@ -6,7 +6,12 @@ using namespace std;
 
 int main() {
 
-    cout<<"Hola Mundo :D"<<endl;
+    int entero = 15;  //entero
+    float flotante = 10.45; //flotante solo dos decimales
+    double mayor = 16.3456; //flotante pero con mas decimales
+    char letra = 'a';//caracter
+
+    cout<<mayor; //imprimir variables
 
     return 0;
 }
