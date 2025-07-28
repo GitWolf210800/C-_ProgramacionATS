757dc33d9cb6931b57d5e250f0ba37db8a8c816a
GitWolf210800 <ezad821q@hotmail.com>
2025-07-28 07:17:19 -0700

    capitulo 7 del curso

--- main.cpp
+++ main.cpp
@@ -9,7 +9,18 @@ su suma, resta, multiplicación y división
 using namespace std;
 
 int main() {
-    int n1, n2, suma = 0;
+    float a, b, c, d, e, f, resultado = 0;
+
+    cout << "Digite el Valor de a"; cin >> a;
+    cout << "Digite el Valor de b"; cin >> b;
+    cout << "Digite el Valor de c"; cin >> c;
+    cout << "Digite el Valor de d"; cin >> d;
+    cout << "Digite el Valor de e"; cin >> e;
+    cout << "Digite el Valor de f"; cin >> f;
+
+    resultado = (a + b / c) / (d + e /f);
+
+    cout << "\n el resultado es: " << resultado << endl;
 
     return 0;
 }
