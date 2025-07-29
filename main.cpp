e882c8e5516a48bda15582b795936bcc19b518aa
GitWolf210800 <ezad821q@hotmail.com>
2025-07-28 21:33:51 -0300

    capitulo 8 ejercicio 6

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 Cuarto programa Escribir un programa que lea de la entrada estandar 
 dos número y muestre en la salida estándar 
 su suma, resta, multiplicación y división
+
+Ejercicio 6:
+Escriba un programa que lea la nota final de cuatro alumnos y calcule l nota final de media de los cuatros alumnos
 */
 
 #include <iostream> //libreria para entrada y salida de datos
@@ -9,13 +12,13 @@ su suma, resta, multiplicación y división
 using namespace std;
 
 int main() {
-    int x, y, aux;
-    cout << "Digite el valor de x: "; cin >> x;
-    cout << "Digite el valor de y: "; cin >> y;
+    int a, b, c, d, promedio = 0;
+    cout << "Digite la nota del alumno a: "; cin >> a;
+    cout << "Digite la nota del alumno b: "; cin >> b;
+    cout << "Digite la nota del alumno c: "; cin >> c;
+    cout << "Digite la nota del alumno d: "; cin >> d;
 
-    aux = x;
-    x = y;
+    promedio = (a + b + c + d) / 4;
 
-    cout<<"\nEl nuevo valor de x es: "<<x<<endl;
-    cout<<"El nuevo valor de y es:"<<y<<endl;
+    cout<<"\nEl nuevo valor del promedio de notas es: "<<promedio<<endl;
 }
