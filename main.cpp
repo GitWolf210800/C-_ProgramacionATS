e7f336fc9aaf01ab08b961fb65139fe0ea10f2f1
GitWolf210800 <ezad821q@hotmail.com>
2025-07-29 21:34:45 -0300

    capitulo 9 ejercicio 8

--- main.cpp
+++ main.cpp
@@ -2,26 +2,28 @@
 La calificacion final de un estidiante es la media ponderada de tres nota:
 La nota de prácticas que cuenta un 30% del total, la nota teórica que cuenta un 60% y la nota de participación que cuenta el 10% restante. 
     Escriba un programa que lea la entrada estándar, las tres notas de un alumno y escriba la salida estándar si nota final.
+
+Ejercicio 9:
+Escriba un programa que lea de la entrada estándar los dos catetos de un triángulo rectángulo y escriba en la salida estándar su hipotenusa
 */
 
 #include <iostream> //libreria para entrada y salida de datos
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    float practica, teorica, participacion, nota_final;
+    int cateto_menor, cateto_mayor, hipotenusa;
 
-    cout << "Digite la nota de practica: "; cin >> practica;
-    cout << "Digite la nota de teorica: "; cin >> teorica;
-    cout << "Digite la nota de participacion: "; cin >> participacion;
+    cout << "Digite el cateto menor: "; cin >> cateto_menor;
+    cout << "Digite el cateto mayor: "; cin >> cateto_mayor;
 
-    practica *= 0.30;
-    teorica *= 0.60;
-    participacion *= 0.10;
+    cateto_menor = pow(cateto_menor, 2);
+    cateto_mayor = pow(cateto_mayor, 2);
 
-    nota_final = practica + teorica + participacion;
+    hipotenusa = sqrt( cateto_menor + cateto_mayor );
 
-    cout << "\nLa nota final es: " <<nota_final;
+    cout << "El valor de la hipotenusa es: " << hipotenusa << endl;
 
     return 0;
 }
