64e78526e7d0945615656ae59e3bb21b67652815
GitWolf210800 <ezad821q@hotmail.com>
2025-08-03 18:21:21 -0300

    progreso del video capitulo 9

--- main.cpp
+++ main.cpp
@@ -1,29 +1,21 @@
 /*
-La calificacion final de un estidiante es la media ponderada de tres nota:
-La nota de prácticas que cuenta un 30% del total, la nota teórica que cuenta un 60% y la nota de participación que cuenta el 10% restante. 
-    Escriba un programa que lea la entrada estándar, las tres notas de un alumno y escriba la salida estándar si nota final.
-
-Ejercicio 9:
-Escriba un programa que lea de la entrada estándar los dos catetos de un triángulo rectángulo y escriba en la salida estándar su hipotenusa
+9. Realice un programa que calcule el valor que toma la siguiente funcion 
 */
 
 #include <iostream> //libreria para entrada y salida de datos
-#include <cmath>
+#include <math.h>
 
 using namespace std;
 
 int main() {
-    int cateto_menor, cateto_mayor, hipotenusa;
+    float x, y, resultado = 0;
 
-    cout << "Digite el cateto menor: "; cin >> cateto_menor;
-    cout << "Digite el cateto mayor: "; cin >> cateto_mayor;
+    cout << "Digite el valor de x: "; cin >> x;
+    cout << "Digite el valor de y: "; cin >> y;
 
-    cateto_menor = pow(cateto_menor, 2);
-    cateto_mayor = pow(cateto_mayor, 2);
+    resultado = (sqrt(x)) / (pow(y, 2) - 1);
 
-    hipotenusa = sqrt( cateto_menor + cateto_mayor );
-
-    cout << "El valor de la hipotenusa es: " << hipotenusa << endl;
+    cout << "\n El resultado es ";
 
     return 0;
 }
